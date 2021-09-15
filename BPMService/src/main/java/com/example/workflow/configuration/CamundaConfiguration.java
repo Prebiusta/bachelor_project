8fe8ed2650684ba5d9ddad8907822e6127a2a1e7
Dorci <280145@gmail.com>
2021-09-15 13:58:39 +0200

    [BPM] disable CORS

---
+++ BPMService/src/main/java/com/example/workflow/configuration/CamundaConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.workflow.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CamundaConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+    }
+}
