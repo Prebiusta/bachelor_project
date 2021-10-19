6044c8d3fe61cff7d9a27d55c0487c880d3e5d5e
Kwoconut <angelsomicu@gmail.com>
2021-10-19 17:49:00 +0200

    ui first iteration

---
+++ signfluent-web/src/app/modules/documents/documents-approve/sf-documents-approve.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+
+@Component({
+    selector: 'sf-documents-delegate',
+    templateUrl: './sf-documents-approve.component.html',
+    styleUrls: ['./sf-documents-approve.component.scss']
+})
+
+export class SfDocumentsApproveComponent {
+
+}
