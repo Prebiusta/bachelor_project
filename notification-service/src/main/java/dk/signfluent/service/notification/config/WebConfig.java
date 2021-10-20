8d09cbec4bac9e009b617be99cb03bae7d6076cb
david <david.franko1998@gmail.com>
2021-10-20 10:15:21 +0200

    B2-8 - Added CORS mapping

--- notification-service/src/main/java/dk/signfluent/service/notification/config/WebConfig.java
+++ notification-service/src/main/java/dk/signfluent/service/notification/config/WebConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/dk.signfluent.service.document.api/**")
+        registry.addMapping("/api/**")
                 .allowedOrigins("*")
                 .allowedHeaders("*")
                 .allowCredentials(false).maxAge(3600);
