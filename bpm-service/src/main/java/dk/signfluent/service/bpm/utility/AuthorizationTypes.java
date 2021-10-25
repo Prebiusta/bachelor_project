74f027de7616c6a1d0d9d5c476f7262a4dcfb2bd
david <david.franko1998@gmail.com>
2021-10-25 18:13:41 +0200

    B2-26 - Added more roles

--- bpm-service/src/main/java/dk/signfluent/service/bpm/utility/AuthorizationTypes.java
+++ bpm-service/src/main/java/dk/signfluent/service/bpm/utility/AuthorizationTypes.java
@@ -2,4 +2,6 @@ package dk.signfluent.service.bpm.utility;
 
 public interface AuthorizationTypes {
     String ADMINISTRATOR = "hasAuthority('administrator')";
+    String APPROVER = "hasAuthority('approver')";
+    String DELEGATOR = "hasAuthority('delegator')";
 }
