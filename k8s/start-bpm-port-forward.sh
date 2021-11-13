bb5aad76d950c9e3ced5a3fffc4b4293fb28a1a7
david <david.franko1998@gmail.com>
2021-11-13 17:07:49 +0100

    B2-21 - Added databases and extracted configs

--- k8s/start-bpm-port-forward.sh
+++ k8s/start-bpm-port-forward.sh
@@ -7,4 +7,5 @@ $FORWARD svc/document-service-svc 8001:8001 \
   & $FORWARD svc/device-service-svc 8005:8005 \
   & $FORWARD svc/notification-service-svc 8002:8002 \
   & $FORWARD svc/ca-svc 8004:8004 \
-  & $FORWARD svc/idp-keycloak 8080:8080
+  & $FORWARD svc/idp-keycloak 8080:8080 \
+  & $FORWARD svc/mysql 3306:3306
