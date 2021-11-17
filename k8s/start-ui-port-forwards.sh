7d935a3a32dafc5597f7755e86a8e098d549d431
david <david.franko1998@gmail.com>
2021-11-17 10:09:23 +0100

    Added postman

--- k8s/start-ui-port-forwards.sh
+++ k8s/start-ui-port-forwards.sh
@@ -2,4 +2,5 @@
 
 FORWARD="kubectl port-forward"
 
-$FORWARD svc/bpm-service-svc 8000:8000 --address='0.0.0.0'
+$FORWARD svc/business-service-svc 8006:8006 --address='0.0.0.0' /
+  $FORWARD svc/idp-keycloak 8080:8080
