f530d7b9798e4cacd1012350bfd1c1ccb90565fb
david <david.franko1998@gmail.com>
2021-12-12 18:13:19 +0100

    B2-21 - Ingress added

--- business-service/src/main/java/dk/signfluent/businessservice/controller/advice/ErrorControllerAdvice.java
+++ business-service/src/main/java/dk/signfluent/businessservice/controller/advice/ErrorControllerAdvice.java
@@ -9,11 +9,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Arrays;
+
 @ControllerAdvice
 @Slf4j
 public class ErrorControllerAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler({Exception.class})
     protected ResponseEntity<ErrorResponse> handleConflict(Exception ex, WebRequest request) {
+        log.error(Arrays.toString(ex.getStackTrace()));
         return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
